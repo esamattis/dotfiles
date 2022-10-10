1e652f65076861924bd18768fcb87ee6e3b5acd6
EsaMatti Suuronen <esamatti1@esamattis-mbp-2.lan>
2022-10-10 11:46:20 +0300

    Add vim

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+set -x
+
+rm -f "$HOME/.vimrc"
+ln -sf "$(pwd)/vim/vimrc" "$HOME/.vimrc"
+
+rm -f "$HOME/.config/nvim/init.vim"
+mkdir -p "$HOME/.config/nvim"
+ln -sf "$(pwd)/vim/vimrc" "$HOME/.config/nvim/init.vim"
