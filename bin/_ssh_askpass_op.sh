6ca3e362d224fe527aff6b9f8ecdc0f4496753f1
Esa-Matti Suuronen <code@esamatti.fi>
2024-11-09 15:13:23 +0200

    nicer ask pass

---
+++ bin/_ssh_askpass_op.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+# $1 is like "Enter passphrase for /Users/esamatti/.ssh/id_ed25519_personal_github:"
+file_path=$(echo "$1" | sed -E 's/.* for (.+): $/\1/')
+ip_id_file="${file_path}.op"
+
+if [ ! -f "$ip_id_file" ]; then
+   >&2 echo "1pw ssh askpass error: no id file: $ip_id_file"
+   exit 1
+fi
+
+op_item_id="$(cat "$ip_id_file")"
+
+>&2 echo "Asking $op_item_id from 1Password"
+exec op item get "$op_item_id" --reveal --fields password
--- bin/ssh_askpass_1pw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-exec op item get "$OP_ITEM_ID" --reveal --fields password
